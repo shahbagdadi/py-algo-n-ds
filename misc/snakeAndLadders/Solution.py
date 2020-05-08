ced62465576e4d8411f8371281776a594e4adc09
Shah Bagdadi <email.shanu@gmail.com>
2020-05-08 13:29:55 -0700

    snake and ladders

---
+++ misc/snakeAndLadders/Solution.py
@@ -0,0 +1,38 @@
+from typing import List
+import collections
+
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        N = len(board)
+
+        def get(s):
+            # Given a square num s, return board coordinates (r, c)
+            quot, rem = divmod(s-1, N)
+            row = N - 1 - quot
+            col = rem if quot%2 == 0 else N - 1 - rem
+            return row, col
+
+        dist = {1: 0}
+        queue = collections.deque([1])
+        while queue:
+            s = queue.popleft()
+            if s == N*N: return dist[s]
+            for s2 in range(s+1, min(s+6, N*N) + 1):
+                r, c = get(s2)
+                if board[r][c] != -1:
+                    s2 = board[r][c]
+                if s2 not in dist:
+                    dist[s2] = dist[s] + 1
+                    queue.append(s2)
+        return -1
+
+s = Solution()
+ip = [
+[-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1],
+[-1,35,-1,-1,13,-1],
+[-1,-1,-1,-1,-1,-1],
+[-1,15,-1,-1,-1,-1]]
+ans = s.snakesAndLadders(ip)
+print(ans)
