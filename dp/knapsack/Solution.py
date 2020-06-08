36bf7a931ad23d767f17b7450884629a8c8fd634
Shah Bagdadi <email.shanu@gmail.com>
2020-06-08 16:09:44 -0700

    unbounded knapsack

--- dp/knapsack/Solution.py
+++ dp/knapsack/Solution.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from functools import lru_cache
 
 class Solution:
 
@@ -18,32 +18,47 @@ class Solution:
         return dp[n][W]
 
     #  T - O(2^n)    S - O(n)
+    #  lru_cache T - O(N * N)    S - O(N)
     def knapSackR(self, wt: List, val: List, W: int) -> int:
+        # @lru_cache(None)
+        def helper( W, n):
+            if n == 0 or W <= 0:  # base conditions
+                return 0 
+            sum1 = 0 
+            if W >= wt[n-1] :
+                sum1 = val[n-1] + helper(W - wt[n-1], n-1)
+            sum2 = helper(W , n-1 )
+            return max(sum1, sum2)
+        return helper(W, len(wt))
 
-        def helper(wt, val, W, n):
-            if n == 0 or W == 0:  # base conditions
-                return 0
-            # If weight is higher than capacity then it is not included
-            if (wt[n-1] > W):
-                return helper(wt, val, W, n-1)
-            # return either nth item being included or not
-            else:
-                return max(val[n-1] + helper(wt, val, W-wt[n-1], n-1), helper(wt, val, W, n-1))
 
-        return helper(wt, val, W, len(wt))
+    def knapSackRUnbound(self, wt: List, val: List, W: int) -> int:
+        @lru_cache(None)
+        def helper( W, n):
+            if n == 0 or W <= 0:  # base conditions
+                return 0   
+            sum1 = 0 
+            if W >= wt[n-1] :
+                sum1 = val[n-1] + helper(W - wt[n-1], n)
+            sum2 = helper(W , n-1 )
+            return max(sum1, sum2)
 
+        return helper(W, len(wt))
 
 s = Solution()
-# wt = [1, 3, 4, 5]
-# val = [1, 4, 5, 7]
-# maxw = 7
+wt = [1, 3, 4, 5]
+val = [3, 4, 5, 7]
+maxw = 7
 
-wt = [1, 1, 1]
-val = [10, 20, 30]
-maxw = 2
+# wt = [1, 1, 1]
+# val = [10, 20, 30]
+# maxw = 2
 
 ans = s.knapSackR(wt, val, maxw)
 print(ans)
 
 ans1 = s.knapSackDP(wt, val, maxw)
+print(ans1)
+
+ans1 = s.knapSackRUnbound(wt, val, maxw)
 print(ans1)
