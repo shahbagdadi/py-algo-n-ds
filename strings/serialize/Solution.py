195571abeb26e8b5a9cccbf87ead3c039eafe4dc
Shah Bagdadi <email.shanu@gmail.com>
2020-06-12 23:35:11 -0700

    handled edge conditions

--- strings/serialize/Solution.py
+++ strings/serialize/Solution.py
@@ -8,17 +8,16 @@ class Solution :
 
  def convertBackToArray(self, encoded : str) -> List[str] :
     meta_data , sep , data = encoded.partition('|')
-    e = 0
+    if not meta_data : return []
     mdata = [ int(s) for s in meta_data.split(',')]
-    e , decoded = 0 , []
+    s , decoded = 0 , []
     for l in mdata:
-        s = e
-        e += l
-        decoded.append(data[s:e])
+        decoded.append(data[s:s+l])
+        s += l
     return decoded
 
 s = Solution()
-ip = ['hello', 'hi' , '' , 'b|ye']
+ip = ['hel,lo', 'hi' , '' , 'b|ye']
 ans1 = s.convertToString(ip)
 print(ans1)
 ans2 = s.convertBackToArray(ans1)
