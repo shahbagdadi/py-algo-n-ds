6fa7adf177af6a33db4d45c36c9a7c89e85d5145
Shah Bagdadi <email.shanu@gmail.com>
2020-07-11 10:22:50 -0700

    subset template

--- backtrack/template/Solution.py
+++ backtrack/template/Solution.py
@@ -14,6 +14,10 @@ class Solution:
         backtrack(0, len(nums), [])
         return ans
 
+    # same as subsets above
+    def combinations(self,l):    
+        if l == []: return [[]]    
+        return self.combinations(l[1:]) + [[l[0]] + c for c in self.combinations(l[1:])]
 
     def permute(self, nums):
         def backtrack(start, end):
@@ -28,9 +32,18 @@ class Solution:
         return ans
 
 
+
 ip = [1,2,3]
 s = Solution()
 sets = s.subsets(ip)
 print(f'Powerset => {sets}')
+# same as above
+comb = s.combinations(ip)
+print(f'Powerset2 => {comb}')
+
+
 perm = s.permute(ip)
 print(f'Permutation => {perm}')
+
+
+
